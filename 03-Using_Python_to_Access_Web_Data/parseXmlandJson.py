48e13e1a01c52435c8f78c7eae837a3df5cfd034
signinvipin <111228283+signinvipin@users.noreply.github.com>
2023-08-28 16:20:17 +0530

    Add parse XML and JSON

---
+++ 03-Using_Python_to_Access_Web_Data/parseXmlandJson.py
@@ -0,0 +1,69 @@
+# Parse eXtensible Markup Language(XML) and JavaScript 
+# Object Notation(JSON) received over TCP/IP socket.
+
+# schema for XML - XSD and other
+'''
+print('>> Parsing eXtensible Markup Language <<')
+'''
+
+# XML data received over internet
+data = '''
+<stuff>
+  <users>
+    <user x="2">
+      <id>001</id>
+      <name>Chuck</name>
+    </user>
+    <user x="7">
+      <id>009</id>
+      <name>Brent</name>
+    </user>
+  </users>
+</stuff>
+'''
+
+'''
+import xml.etree.ElementTree as ET
+
+# Convert data from string to tree node
+data = ET.fromstring(data)
+
+# find all user tags under users
+users = data.findall('users/user')
+
+# loop-over all user list users
+for user in users:
+    # find tag name and get the text inside
+    print('Name: ',user.find('name').text)
+    # get the attribute in tag user
+    print('Attribute: ',user.get('x'))
+
+'''
+'''
+# Parsing JSON(JavaScript Object Notation)
+print('>>> Parsing JSON <<<')
+print('>> JSON is a form of dictionary. <<')
+'''
+'''
+data = '''
+[
+  { "id" : "001",
+    "x" : "2",
+    "name" : "Chuck"
+  } ,
+  { "id" : "009",
+    "x" : "7",
+    "name" : "Brent"
+  }
+]'''
+'''
+'''
+import json
+
+dataObj = json.loads(data)
+
+for user in dataObj:
+    print('name : ',user['name'])
+    print('id : ',user['id'])
+
+'''
